3ab952ca0daf873df57a72cd67c976776d43b0fc
Priyanshu Gahlot <anshu2084gahlot@gmail.com>
2023-09-23 02:16:08 +0530

    sounds,rockfalling,particles

---
+++ Assets/_Scripts/SoundOncollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOncollision : MonoBehaviour
+{
+    public string soundName;
+    SoundManager soundManager;
+
+    private void Start()
+    {
+        soundManager = FindObjectOfType<SoundManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        soundManager.Play(gameObject,soundName,1,false);
+    }
+}
